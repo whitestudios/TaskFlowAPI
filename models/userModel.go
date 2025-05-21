24f105f20b951cb1f57d96c633c63ed4f2a01a69
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-21 14:49:55 -0300

    feat: update userModel and add a modular handle system

--- models/userModel.go
+++ models/userModel.go
@@ -2,8 +2,8 @@ package models
 
 type User struct {
 	ID       uint   `gorm:"primaryKey"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" gorm:"not null"`
+	Email    string `json:"email" gorm:"unique; not null"`
+	Password string `json:"password" gorm:"not null"`
 	Tasks    []Task // One (user) to many (tasks)
 }
--- router/routes.go
+++ router/routes.go
@@ -4,10 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/whitestudios/TaskFlowAPI/handlers/user"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	BasePath := "/api"
+
+	user.Init()
 
 	api := router.Group(BasePath)
 	{
@@ -15,4 +18,42 @@ func initializeRoutes(router *gin.Engine) {
 			c.JSON(http.StatusOK, gin.H{"msg": "Hello World!"})
 		})
 	}
+
+	// TODO: Implement User routes
+	usersGroup := api.Group("/user")
+	{
+		usersGroup.GET("", user.ShowUserHandler)
+
+		usersGroup.POST("", user.CreateUserHandler)
+
+		usersGroup.PUT("", user.UpdateUserHandler)
+
+		usersGroup.DELETE("", user.DeleteUserHandler)
+
+		usersGroup.GET("/all", user.ListUserHandler)
+	}
+	// Create task's routes
+	task := api.Group("/task")
+	{
+		task.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "Task Info"})
+		})
+
+		task.POST("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "Task Created"})
+		})
+
+		task.PUT("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "Task Updated"})
+		})
+
+		task.DELETE("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "Task Deleted"})
+		})
+
+		task.GET("/all", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"msg": "Tasks"})
+		})
+	}
+
 }
